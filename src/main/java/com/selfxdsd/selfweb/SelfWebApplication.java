03bf38f3ba50ee3a3800de1fe142bbb0933483e6
amihaiemil <amihaiemil@gmail.com>
2020-05-26 13:57:55 +0300

    #14 checkstyle fixes

--- src/main/java/com/selfxdsd/selfweb/SelfWebApplication.java
+++ src/main/java/com/selfxdsd/selfweb/SelfWebApplication.java
@@ -34,7 +34,6 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.Map;
 
 /**
