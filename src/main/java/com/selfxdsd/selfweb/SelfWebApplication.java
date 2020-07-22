47ae2c69142fec2e68e97da687b416fad2401c47
amihaiemil <amihaiemil@gmail.com>
2020-07-22 21:33:59 +0300

    #27 Users API, moved /user to /users/self

--- src/main/java/com/selfxdsd/selfweb/SelfWebApplication.java
+++ src/main/java/com/selfxdsd/selfweb/SelfWebApplication.java
@@ -27,14 +27,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 /**
  * Spring Boot entry point.
@@ -44,21 +38,7 @@ import java.util.Map;
  * @checkstyle HideUtilityClassConstructor (100 lines)
  */
 @SpringBootApplication
-@RestController
 public class SelfWebApplication extends WebSecurityConfigurerAdapter {
-
-    /**
-     * Get the authenticated user from the session.
-     * @param principal Authenticated user.
-     * @return Map.
-     */
-    @GetMapping("/user")
-    public Map<String, Object> user(
-        @AuthenticationPrincipal
-        final OAuth2User principal
-    ) {
-        return principal.getAttributes();
-    }
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
