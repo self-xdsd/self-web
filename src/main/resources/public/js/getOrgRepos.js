f41f048c7564084a999ad14691148a05b120cf38
amihaiemil <amihaiemil@gmail.com>
2020-07-26 23:38:02 +0300

    #37 org repos table scaffolding

---
+++ src/main/resources/public/js/getOrgRepos.js
@@ -0,0 +1,30 @@
+$.get(
+    "/api/repositories/orgs",
+    function(repos) {
+        repos.sort(
+            function(one, other) {
+                if(one.fork == true) {
+                    return 1
+                }
+                if(other.fork == true) {
+                    return -1
+                }
+                return 0
+            }
+        ).forEach(
+            function(repo){
+                $("#orgReposTable").find("tbody").append(repoAsTableRow(repo));
+            }
+        )
+        $('#orgReposTable').dataTable();
+    }
+);
+
+/**
+ * Wrap a repo's information between <li> tags, with anchor.
+ */
+function repoAsTableRow(repo) {
+    return "<tr><td>" +
+        repo.full_name
+        + "</td></tr>"
+}
