58cb2f59f99f65f5114692d47c92a33f445a9577
amihaiemil <amihaiemil@gmail.com>
2020-07-28 13:52:49 +0300

    #37 load repos on document ready

--- src/main/resources/public/js/getOrgRepos.js
+++ src/main/resources/public/js/getOrgRepos.js
@@ -1,26 +1,37 @@
-$("#loadingOrgRepos").show();
-$.get(
-    "/api/repositories/orgs",
-    function(repos) {
-        repos.sort(
-            function(one, other) {
-                if(one.fork == true) {
-                    return 1
-                }
-                if(other.fork == true) {
-                    return -1
-                }
-                return 0
-            }
-        ).forEach(
-            function(repo){
-                $("#orgReposTable").find("tbody").append(repoAsTableRow(repo));
-            }
-        )
-        $("#loadingOrgRepos").hide();
-        $('#orgReposTable').dataTable();
+$(document).ready(
+    function () {
+        getOrgRepos();
     }
 );
+/**
+ * Get Org Repos to which the authenticated user has admin rights.
+ */
+function getOrgRepos() {
+    $("#loadingOrgRepos").show();
+    $.get(
+        "/api/repositories/orgs",
+        function(repos) {
+            repos.sort(
+                function(one, other) {
+                    if(one.fork == true) {
+                        return 1
+                    }
+                    if(other.fork == true) {
+                        return -1
+                    }
+                    return 0
+                }
+            ).forEach(
+                function(repo){
+                    $("#orgReposTable").find("tbody").append(repoAsTableRow(repo));
+                }
+            )
+            $("#loadingOrgRepos").hide();
+            $('#orgReposTable').dataTable();
+        }
+    );
+
+}
 
 /**
  * Wrap a repo's information between <li> tags, with anchor.
