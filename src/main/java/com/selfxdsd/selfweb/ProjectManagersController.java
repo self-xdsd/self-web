052569647f06f30b14522d86a8df79bb324937d3
amihaiemil <amihaiemil@gmail.com>
2020-08-12 17:34:34 +0300

    admin page for PMs

---
+++ src/main/java/com/selfxdsd/selfweb/ProjectManagersController.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2020, Self XDSD Contributors
+ * All rights reserved.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.selfxdsd.selfweb;
+
+import com.selfxdsd.api.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Project Managers page Controller.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 0.0.1
+ */
+@Controller
+public class ProjectManagersController {
+
+    /**
+     * Authenticated user.
+     */
+    private User user;
+
+    /**
+     * Constructor.
+     * @param user Authenticated User.
+     */
+    @Autowired
+    public ProjectManagersController(final User user) {
+        this.user = user;
+    }
+
+    /**
+     * Serve the PMs page (only for admins).
+     * @return PMs page.
+     */
+    @GetMapping("/admin/pms")
+    public String projectManagers() {
+        final String page;
+        if("admin".equals(user.role())) {
+            page = "projectManagers.html";
+        } else {
+            page = "index.html";
+        }
+        return page;
+    }
+}
