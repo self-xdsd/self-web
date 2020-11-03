389dd32e7bf1dca6caaf3711f57bf69cb4e64b8c
Lumi3011 <lumi.3011@yahoo.ro>
2020-11-03 17:23:32 +0200

    Final modifications

--- src/test/java/com/selfxdsd/selfweb/api/output/JsonProjectTestCase.java
+++ src/test/java/com/selfxdsd/selfweb/api/output/JsonProjectTestCase.java
@@ -1,7 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (c) 2020, Self XDSD Contributors
+ * All rights reserved.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 package com.selfxdsd.selfweb.api.output;
 
@@ -22,6 +39,7 @@ import org.hamcrest.MatcherAssert;
 
 
 /**
+ * Unit tests for {@link JsonProject}
  * @author Lumi
  */
 public final class JsonProjectTestCase {
@@ -29,8 +47,7 @@ public final class JsonProjectTestCase {
 
     /**
      * JsonProject has repoFullName
-     */
-    
+     */    
     @Test
     public void hasRepoFullName(){
         final Project project = Mockito.mock(Project.class);
@@ -78,9 +95,7 @@ public final class JsonProjectTestCase {
 }
     /**
      * JsonProject has Provider
-     */
-    
-    
+     */   
     @Test
     public void hasProvider(){
     
@@ -130,8 +145,6 @@ public final class JsonProjectTestCase {
     /**
      * JsonProject has Owner
      */
-    
-    
     @Test
     public void hasOwner(){
     
@@ -182,8 +195,6 @@ public final class JsonProjectTestCase {
     /**
      * JsonProject has Manager
      */
-    
-    
     @Test
     public void hasManager(){
     
@@ -246,8 +257,6 @@ public final class JsonProjectTestCase {
     /**
      * JsonProject has Wallet
      */
-    
-    
     @Test
     public void hasWallet(){
     
