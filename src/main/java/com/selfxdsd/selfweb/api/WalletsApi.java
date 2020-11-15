46d77cf22c5301e5731a5e9264e3587d15563684
cristianpela <cristypela05@gmail.com>
2020-11-15 11:10:06 +0200

    #137 Wallet activation endpoint implemented+tested.

--- src/main/java/com/selfxdsd/selfweb/api/WalletsApi.java
+++ src/main/java/com/selfxdsd/selfweb/api/WalletsApi.java
@@ -26,6 +26,7 @@ import com.selfxdsd.api.Project;
 import com.selfxdsd.api.Self;
 import com.selfxdsd.api.User;
 import com.selfxdsd.api.Wallet;
+import com.selfxdsd.api.Wallets;
 import com.selfxdsd.api.exceptions.WalletAlreadyExistsException;
 import com.selfxdsd.selfweb.api.output.JsonWallet;
 import com.selfxdsd.selfweb.api.output.JsonWallets;
@@ -37,6 +38,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -46,11 +50,11 @@ import java.math.RoundingMode;
  * @author Mihai Andronache (amihaiemil@gmail.com)
  * @version $Id$
  * @since 0.0.1
- * @todo #135:30min Add an endpoint and front-end code for a
- *  Wallet's activation.
  * @todo #135:60min Once the PaymentMethod logic is implemented in the core,
  *  we should add a form in the Real Wallet widget, where the User will add
  *  payment methods.
+ * @todo #178:30min On frontend, in `getProject.js`, integrate the wallet
+ *  activation feature with the backend.
  */
 @RestController
 @Validated
@@ -199,6 +203,65 @@ public class WalletsApi extends BaseApiController {
                         new JsonWallet(wallet.updateCash(cash)).toString()
                     );
                 }
+            }
+        }
+        return response;
+    }
+
+    /**
+     * Activates a Wallet. Returns an array of wallet types which includes the
+     * activated one and the other types deactivated.
+     * @param owner Owner of the project (login of user or org name).
+     * @param name Repo name.
+     * @param type Wallet type (ex: stripe).
+     * @return Array of wallet types which includes the activated one and the
+     * other types deactivated.
+     */
+    @PutMapping(
+        value = "/projects/{owner}/{name}/wallets/{type}/activate",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<String> activate(
+        @PathVariable final String owner,
+        @PathVariable final String name,
+        @PathVariable final String type){
+
+        final ResponseEntity<String> response;
+        final Project project = this.self.projects().getProjectById(
+            owner + "/" + name, user.provider().name()
+        );
+        if (project == null) {
+            response = ResponseEntity.badRequest()
+                .body("Project not found");
+        } else {
+            final Wallets wallets = project.wallets();
+            Wallet wallet = null;
+            for (final Wallet search : wallets) {
+                if (search.type().equals(type)) {
+                    wallet = search;
+                    break;
+                }
+            }
+            if (wallet == null) {
+                response = ResponseEntity.badRequest()
+                    .body("Wallet of type " + type + " not found.");
+            } else if (wallet.active()){
+                response = ResponseEntity.badRequest()
+                    .body("Wallet of type " + type + " already active");
+            } else {
+                wallets.activate(wallet);
+                final JsonArray walletTypes = new JsonWallets(wallets)
+                    .stream()
+                    .map(w -> Json.createObjectBuilder()
+                        .add("type", ((JsonWallet) w).get("type"))
+                        .add("active", ((JsonWallet) w).get("active"))
+                        .build())
+                    .reduce(
+                        Json.createArrayBuilder(),
+                        JsonArrayBuilder::add,
+                        (comb, curr) -> comb
+                    ).build();
+                response = ResponseEntity.ok(walletTypes.toString());
             }
         }
         return response;
--- src/test/java/com/selfxdsd/selfweb/api/WalletsApiTestCase.java
+++ src/test/java/com/selfxdsd/selfweb/api/WalletsApiTestCase.java
@@ -31,10 +31,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonObject;
 import java.io.StringReader;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -175,6 +177,179 @@ public final class WalletsApiTestCase {
 
         final ResponseEntity<String> resp = api
             .updateCash("john", "test", Wallet.Type.STRIPE, 10.5f);
+        MatcherAssert.assertThat(resp.getStatusCode(),
+            Matchers.is(HttpStatus.BAD_REQUEST));
+    }
+
+    /**
+     * WalletsApi.activate(...) activates a wallet.
+     * @checkstyle ExecutableStatementCount (100 lines)
+     */
+    @Test
+    public void activateWalletWorks(){
+        final Self self = Mockito.mock(Self.class);
+
+        final User user = Mockito.mock(User.class);
+        final Provider provider = Mockito.mock(Provider.class);
+        Mockito.when(provider.name()).thenReturn(Provider.Names.GITHUB);
+        Mockito.when(user.provider()).thenReturn(provider);
+
+        final Projects projects = Mockito.mock(Projects.class);
+        final Project project = Mockito.mock(Project.class);
+        Mockito.when(self.projects()).thenReturn(projects);
+        Mockito.when(projects.getProjectById(
+            "john/test",
+            Provider.Names.GITHUB
+        )).thenReturn(project);
+
+        final Wallet fake = Mockito.mock(Wallet.class);
+        Mockito.when(fake.type()).thenReturn(Wallet.Type.FAKE);
+        Mockito.when(fake.cash()).thenReturn(BigDecimal.TEN);
+        Mockito.when(fake.available()).thenReturn(BigDecimal.TEN);
+        Mockito.when(fake.debt()).thenReturn(BigDecimal.ZERO);
+        final Wallet stripe = Mockito.mock(Wallet.class);
+        Mockito.when(stripe.type()).thenReturn(Wallet.Type.STRIPE);
+        Mockito.when(stripe.cash()).thenReturn(BigDecimal.TEN);
+        Mockito.when(stripe.available()).thenReturn(BigDecimal.TEN);
+        Mockito.when(stripe.debt()).thenReturn(BigDecimal.ZERO);
+
+        final Wallets wallets = Mockito.mock(Wallets.class);
+        final List<Wallet> walletsSrc = new ArrayList<>(List.of(fake, stripe));
+        Mockito.when(wallets.spliterator())
+            .thenReturn(walletsSrc.spliterator());
+        Mockito.when(wallets.iterator())
+            .thenReturn(walletsSrc.iterator());
+        Mockito.when(project.wallets()).thenReturn(wallets);
+        Mockito.when(wallets.activate(stripe)).thenAnswer(inv -> {
+            final Wallet activated = Mockito.mock(Wallet.class);
+            Mockito.when(activated.type()).thenReturn(Wallet.Type.STRIPE);
+            Mockito.when(activated.cash()).thenReturn(BigDecimal.TEN);
+            Mockito.when(activated.available()).thenReturn(BigDecimal.TEN);
+            Mockito.when(activated.debt()).thenReturn(BigDecimal.ZERO);
+            Mockito.when(activated.active()).thenReturn(true);
+            walletsSrc.set(1, activated);
+            return activated;
+        });
+
+        final WalletsApi api = new WalletsApi(user, self);
+
+        final ResponseEntity<String> resp =
+            api.activate("john", "test", Wallet.Type.STRIPE);
+
+        MatcherAssert.assertThat(resp.getStatusCode(),
+            Matchers.is(HttpStatus.OK));
+        final JsonArray body = Json.createReader(
+            new StringReader(resp.getBody())
+        ).readArray();
+        MatcherAssert.assertThat(body, Matchers.equalTo(
+            Json.createArrayBuilder()
+                .add(Json.createObjectBuilder()
+                    .add("type", Wallet.Type.FAKE)
+                    .add("active", false)
+                    .build())
+                .add(Json.createObjectBuilder()
+                    .add("type", Wallet.Type.STRIPE)
+                    .add("active", true)
+                    .build())
+                .build()
+        ));
+
+        Mockito.verify(wallets, Mockito.times(1))
+            .activate(stripe);
+    }
+
+    /**
+     * WalletsApi.activate(...) returns error if project is not found.
+     */
+    @Test
+    public void activateReturnsErrorIfProjectNotFound(){
+        final Self self = Mockito.mock(Self.class);
+
+        final User user = Mockito.mock(User.class);
+        final Provider provider = Mockito.mock(Provider.class);
+        Mockito.when(provider.name()).thenReturn(Provider.Names.GITHUB);
+        Mockito.when(user.provider()).thenReturn(provider);
+
+        final Projects projects = Mockito.mock(Projects.class);
+        Mockito.when(self.projects()).thenReturn(projects);
+
+        final WalletsApi api = new WalletsApi(user, self);
+
+        final ResponseEntity<String> resp = api
+            .activate("john", "test", Wallet.Type.STRIPE);
+        MatcherAssert.assertThat(resp.getStatusCode(),
+            Matchers.is(HttpStatus.BAD_REQUEST));
+    }
+
+    /**
+     * WalletsApi.activate(...) returns error if wallet is not found.
+     */
+    @Test
+    public void activateReturnsErrorIfWalletNotFound(){
+        final Self self = Mockito.mock(Self.class);
+
+        final User user = Mockito.mock(User.class);
+        final Provider provider = Mockito.mock(Provider.class);
+        Mockito.when(provider.name()).thenReturn(Provider.Names.GITHUB);
+        Mockito.when(user.provider()).thenReturn(provider);
+
+        final Projects projects = Mockito.mock(Projects.class);
+        final Project project = Mockito.mock(Project.class);
+        Mockito.when(self.projects()).thenReturn(projects);
+        Mockito.when(projects.getProjectById(
+            "john/test",
+            Provider.Names.GITHUB
+        )).thenReturn(project);
+
+        final Wallets wallets = Mockito.mock(Wallets.class);
+        Mockito.when(wallets.iterator())
+            .thenReturn(List.<Wallet>of().iterator());
+        Mockito.when(project.wallets()).thenReturn(wallets);
+
+        final WalletsApi api = new WalletsApi(user, self);
+
+        final ResponseEntity<String> resp = api
+            .activate("john", "test", Wallet.Type.STRIPE);
+        MatcherAssert.assertThat(resp.getStatusCode(),
+            Matchers.is(HttpStatus.BAD_REQUEST));
+    }
+
+    /**
+     * WalletsApi.activate(...) returns error if wallet is already active.
+     */
+    @Test
+    public void activateReturnsErrorIfWalletIsAlreadyActive(){
+        final Self self = Mockito.mock(Self.class);
+
+        final User user = Mockito.mock(User.class);
+        final Provider provider = Mockito.mock(Provider.class);
+        Mockito.when(provider.name()).thenReturn(Provider.Names.GITHUB);
+        Mockito.when(user.provider()).thenReturn(provider);
+
+        final Projects projects = Mockito.mock(Projects.class);
+        final Project project = Mockito.mock(Project.class);
+        Mockito.when(self.projects()).thenReturn(projects);
+        Mockito.when(projects.getProjectById(
+            "john/test",
+            Provider.Names.GITHUB
+        )).thenReturn(project);
+
+        final Wallet stripe = Mockito.mock(Wallet.class);
+        Mockito.when(stripe.type()).thenReturn(Wallet.Type.STRIPE);
+        Mockito.when(stripe.active()).thenReturn(true);
+
+        final Wallets wallets = Mockito.mock(Wallets.class);
+        final List<Wallet> walletsSrc = List.of(stripe);
+        Mockito.when(wallets.spliterator())
+            .thenReturn(walletsSrc.spliterator());
+        Mockito.when(wallets.iterator())
+            .thenReturn(walletsSrc.iterator());
+        Mockito.when(project.wallets()).thenReturn(wallets);
+
+        final WalletsApi api = new WalletsApi(user, self);
+
+        final ResponseEntity<String> resp = api
+            .activate("john", "test", Wallet.Type.STRIPE);
         MatcherAssert.assertThat(resp.getStatusCode(),
             Matchers.is(HttpStatus.BAD_REQUEST));
     }
