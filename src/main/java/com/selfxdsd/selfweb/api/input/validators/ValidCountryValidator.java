a3064f9f8e224e4fce061db2c48c32f167fd77ec
amihaiemil <amihaiemil@gmail.com>
2020-12-23 18:20:31 +0200

    #284 backend only Stripe countries are valid

--- src/main/java/com/selfxdsd/selfweb/api/input/validators/ValidCountryValidator.java
+++ src/main/java/com/selfxdsd/selfweb/api/input/validators/ValidCountryValidator.java
@@ -38,10 +38,13 @@ public final class ValidCountryValidator
     implements ConstraintValidator<ValidCountry, String> {
 
     /**
-     * Forbidden countries (Stripe regulations): Syria, Iran, N. Korea, Cuba.
+     * Stripe countries. We can only work with countries supported by Stripe.
      */
-    private List<String> forbiddenCountries = List.of(
-        "SY", "IR", "KP", "CU"
+    private List<String> stripeCountries = List.of(
+        "AU", "AT", "BE", "BG", "CA", "CY", "CZ", "DK", "EE", "FI", "FR",
+        "DE", "GR", "HK", "HU", "IE", "IT", "JP", "LV", "LT", "LU", "MT",
+        "NL", "NZ", "NO", "PL", "PT", "RO", "SG", "SK", "SI", "ES", "SE",
+        "CH", "GB", "US"
     );
 
     @Override
@@ -49,11 +52,8 @@ public final class ValidCountryValidator
         final String value,
         final ConstraintValidatorContext context
     ) {
-        if(this.forbiddenCountries.contains(value)) {
-            return false;
-        }
-        for(final String country : Locale.getISOCountries()) {
-            if(country.equals(value)) {
+        for(final String country : this.stripeCountries) {
+            if(country.equalsIgnoreCase(value)) {
                 return true;
             }
         }
