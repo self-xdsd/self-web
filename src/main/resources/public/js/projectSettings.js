8e3bc6280bb25c58520b501a3f8861b42ddc81d2
amihaiemil <amihaiemil@gmail.com>
2020-12-27 11:30:24 +0200

    #290 canDeleteProject JS function

--- src/main/resources/public/js/projectSettings.js
+++ src/main/resources/public/js/projectSettings.js
@@ -1,4 +1,25 @@
+/**
+ * Check if a Project can be deleted or not.
+ * If it can be deleted, enable the "Delete Project" button from the Settings tab.
+ */
 function canDeleteProject() {
     $("#deleteProjectLoading").show();
-    return true;
+    $.get({
+        url: "/api/projects/"+$("#owner").text()+"/"+$("#name").text()+"/contracts/count",
+        success: function(response){
+            if(response.contractsCount > 0) {
+                $("#deleteProjectButton").attr("disabled");
+                $("#deleteProjectButton").attr("aria-disabled", "true");
+            } else {
+                $("#deleteProjectButton").removeAttr("disabled");
+                $("#deleteProjectButton").attr("aria-disabled", "false");
+            }
+            $("#deleteProjectLoading").hide();
+        },
+        error: function(jqXHR){
+            console.error("Contracts count returned status: " + jqXHR.status);
+            console.error("Contracts count returned body: " + jqXHR.responseText);
+            $("#deleteProjectLoading").hide();
+        }
+    });
 }
