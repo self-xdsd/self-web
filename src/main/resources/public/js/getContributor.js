bf2d7d65aa194abf2d3d1c4e004ea88c1dcd5bab
cristianpela <cristypela05@gmail.com>
2020-12-31 15:44:33 +0200

    #297 Tables loading improvement on Contributor dashboard.

--- src/main/resources/public/js/getContributor.js
+++ src/main/resources/public/js/getContributor.js
@@ -1,60 +1,62 @@
-/**
- * @todo #294:60min Better table loading approach for Contributor dashboard tables.
- *  Use the same approach specified at issue #294.
- */
 function getContributorDashboard() {
     $("#loadingContributor").show();
-    $.ajax("/api/contributor", {
-        type: "GET",
-        statusCode: {
-            200: function (contributor) {
-                $("#loadingContributor").hide();
-                $("#contributorCardFooter").show();
-                $("#contributorDashboard").removeClass("d-none");
-                $("#contributorDashboard").addClass("show");
-                contributor.contracts.forEach(
-                    function(contract){
-                        $("#contractsTable").find("tbody").append(
-                            contractAsTableRow(contract)
-                        );
-                        $($(".contractAgenda")[$(".contractAgenda").length -1]).on(
-                            "click",
-                            function(event) {
+    $("#contractsTable").dataTable({
+        language: {
+            loadingRecords: '<img src="/images/loading.svg" height="100">'
+        },
+        ajax: function (_data, callback) {
+            $.ajax("/api/contributor", {
+                type: "GET",
+                statusCode: {
+                    200: function (contributor) {
+                        callback({ data: contributor.contracts.map(contractAsTableRow) });
+                        $("#loadingContributor").hide();
+                        $("#contributorCardFooter").show();
+                        $("#contributorDashboard").removeClass("d-none");
+                        $("#contributorDashboard").addClass("show");
+                        $("#contractsTable .contractAgenda").each(function () {
+                            $(this).on("click", () => {
                                 getTasksOfContract(contract);
                                 getInvoicesOfContract(contract);
-                            }
-                        );
-                        $($(".removeContract")[$(".removeContract").length -1]).on(
-                            "click",
-                            function(event) {
+                            });
+                        });
+                        $("#contractsTable .removeContract").each(function () {
+                            $(this).on("click", function(event){
                                 event.preventDefault();
                                 var removeButton = $(this);
+                                var data = $("#contractsTable").DataTable().row($(this).parents('tr')).data()
+                                var contract = {
+                                    id: {
+                                        repoFullName: $(data[0]).text(),
+                                        role: data[1]
+                                    }
+                                }
                                 confirmDialog
                                     .create("Are you sure you want to remove this contract?")
                                     .then(() => markContractForRemoval(contract, removeButton));
-                            }
-                        );
+                            });
+                        });
+                        $('[data-toggle="tooltip"]').tooltip({
+                            boundary: 'window'
+                        });
+                        if (contributor.contracts.length > 0) {
+                            $("#tasks").show();
+                            getTasksOfContract(contributor.contracts[0]);
+                            $("#invoices").show();
+                            getInvoicesOfContract(contributor.contracts[0]);
+                        }
+                        //if the user is redirected by Stripe, we should display the "Payout Methods" tab
+                        if (getUrlVars().includes("stripe")) {
+                            $("#payoutMethodsButton").trigger("click");
+                        }
+                    },
+                    204: function () {
+                        callback({ data: [] });
+                        $("#loadingContributor").hide();
+                        $("#notContributorCardFooter").show();
                     }
-                )
-                $('[data-toggle="tooltip"]').tooltip();
-                if(contributor.contracts.length > 0) {
-                    $("#tasks").show();
-                    getTasksOfContract(contributor.contracts[0]);
-                    $("#invoices").show();
-                    getInvoicesOfContract(contributor.contracts[0]);
                 }
-                $("#contractsTable").dataTable();
-
-                //if the user is redirected by Stripe, we should display the "Payout Methods" tab
-                if(getUrlVars().includes("stripe")) {
-                    $("#payoutMethodsButton").trigger("click");
-                }
-            },
-            204: function (data) {
-                $("#loadingContributor").hide();
-                $("#notContributorCardFooter").show();
-            },
-
+            });
         }
     });
 }
@@ -101,17 +103,15 @@ function contractAsTableRow(contract) {
             + "data-original-title='" + toolTipMessage + "'>"
             +"</i>"
     }
-    return "<tr>" +
-        "<td><a href='" + link + "' target='_blank'>" + contract.id.repoFullName + "</a></td>" +
-        "<td>" + contract.id.role + "</td>"  +
-        "<td>" + contract.hourlyRate + "</td>"  +
-        "<td>" + contract.revenue + "</td>" +
-        "<td><a href='#tasks' title='See Tasks & Invoices' class='contractAgenda'>" +
-            "<i class='fa fa-laptop fa-lg'></i>" +
-        "</a>  "
-        + removeIcon + " " + walletIcon +
-        "</td>" +
-        "</tr>"
+    return [
+        "<a href='" + link + "' target='_blank'>" + contract.id.repoFullName + "</a>",
+        contract.id.role,
+        contract.hourlyRate,
+        contract.revenue,
+        "<a href='#tasks' title='See Tasks & Invoices' class='contractAgenda'>" +
+        "<i class='fa fa-laptop fa-lg'></i>" +
+        "</a>  " + removeIcon + " " + walletIcon
+    ]
 }
 
 /**
@@ -120,69 +120,61 @@ function contractAsTableRow(contract) {
  */
 function getTasksOfContract(contract) {
     $("#tasksTable").dataTable().fnDestroy();
-    $("#tasksTable").find("tbody").html('');
     $("#tasksTitle").html(
         " Tasks in " + contract.id.repoFullName + " (" + contract.id.role + ")"
     )
-    $("#loadingTasks").show();
-    $.ajax( //API call to get the Tasks.
-        "/api/contributor/contracts/"
-        + contract.id.repoFullName
-        + "/tasks?role=" + contract.id.role,
-        {
-            type: "GET",
-            statusCode: {
-                200: function (tasks) {
-                    $("#loadingTasks").hide();
-                    tasks.forEach(
-                        function(task) {
-                            $("#tasksTable").find("tbody").append(
-                                taskAsTableRow(contract, task)
-                            );
+    $("#tasksTable").dataTable({
+        language: {
+            loadingRecords: '<img src="/images/loading.svg" height="100">',
+            emptyTable: "You don't have any tasks assigned."
+        },
+        ajax: function (_data, callback) {
+            $.ajax( //API call to get the Tasks.
+                "/api/contributor/contracts/"
+                + contract.id.repoFullName
+                + "/tasks?role=" + contract.id.role,
+                {
+                    type: "GET",
+                    statusCode: {
+                        200: function (tasks) {
+                            callback({ data: tasks.map(taskAsTableRow(contract)) });
                         }
-                    );
-                    $("#tasksTable").dataTable();
-                    $("#tasksBody").show();
-                    console.log(tasks);
-                },
-                204: function (data) {
-                    $("#loadingTasks").hide();
-                    $("#loadingTasksHidden").show();
-                    $("#tasksTable").dataTable();
-                    $("#tasksBody").show();
-                },
-
-            }
+                    }
+                }
+            );
         }
-    );
+    });
+
 }
 
 /**
  * Turn a Task into a table row.
  * @param task Task.
  */
-function taskAsTableRow(contract, task) {
-    var issueLink;
-    if(contract.id.provider == 'github') {
-        issueLink = 'https://github.com/'
-            + contract.id.repoFullName
-            + "/issues/"
-            + task.issueId;
-    } else if(contract.id.provider == 'gitlab') {
-        issueLink = 'https://gitlab.com/'
-            + contract.id.repoFullName
-            + "/issues/"
-            + task.issueId;
-    } else {
-        issueLink = '#';
-    }
-    return "<tr>" +
-        "<td><a href='" + issueLink + "' target='_blank'>#" + task.issueId + "</a></td>" +
-        "<td>" + task.assignmentDate.split('T')[0] + "</td>"  +
-        "<td>" + task.deadline.split('T')[0] + "</td>" +
-        "<td>" + task.estimation + " min</td>" +
-        "<td>" + formatEuro(task.value) + "</td>" +
-        "</tr>"
+function taskAsTableRow(contract) {
+    return function (task) {
+        var issueLink;
+        if (contract.id.provider == 'github') {
+            issueLink = 'https://github.com/'
+                + contract.id.repoFullName
+                + "/issues/"
+                + task.issueId;
+        } else if (contract.id.provider == 'gitlab') {
+            issueLink = 'https://gitlab.com/'
+                + contract.id.repoFullName
+                + "/issues/"
+                + task.issueId;
+        } else {
+            issueLink = '#';
+        }
+        return [
+            "<a href='" + issueLink + "' target='_blank'>#" + task.issueId + "</a>",
+            task.assignmentDate.split('T')[0],
+            task.deadline.split('T')[0],
+            task.estimation + " min.",
+            formatEuro(task.value),
+        ]
+    };
 }
 
 /**
@@ -191,69 +183,61 @@ function taskAsTableRow(contract, task) {
  */
 function getInvoicesOfContract(contract) {
     $("#invoicesTable").dataTable().fnDestroy();
-    $("#invoicesTable").find("tbody").html('');
     $("#invoicesTitle").html(
         "Invoices of " + contract.id.repoFullName + " (" + contract.id.role + ")"
     )
-    $("#loadingInvoices").show();
-    $.ajax( //API call to get the Invoices.
-        "/api/contributor/contracts/"
-        + contract.id.repoFullName
-        + "/invoices?role=" + contract.id.role,
-        {
-            type: "GET",
-            statusCode: {
-                200: function (invoices) {
-                    $("#loadingInvoices").hide();
-                    invoices.forEach(
-                        function(invoice) {
-                            $("#invoicesTable").find("tbody").append(
-                                invoiceAsTableRow(contract, invoice)
-                            );
+    $("#invoicesTable").dataTable({
+        language: {
+            loadingRecords: '<img src="/images/loading.svg" height="100">'
+        },
+        ajax: function (_data, callback) {
+            $.ajax( //API call to get the Invoices.
+                "/api/contributor/contracts/"
+                + contract.id.repoFullName
+                + "/invoices?role=" + contract.id.role,
+                {
+                    type: "GET",
+                    statusCode: {
+                        200: function (invoices) {
+                            callback({ data: invoices.map(invoiceAsTableRow(contract)) });
                         }
-                    );
-                    $("#invoicesTable").dataTable();
-                    $("#invoicesBody").show();
-                    console.log(invoices);
-                },
-                204: function (data) {
-                    $("#loadingInvoices").hide();
-                    $("#invoicesTable").dataTable();
-                },
-
-            }
+                    }
+                }
+            );
         }
-    );
+    });
 }
 
 /**
  * Turn an Invoice into a table row.
  * @param invoice Invoice.
  */
-function invoiceAsTableRow(contract, invoice) {
-    var transactionId;
-    if(invoice.transactionId == "null") {
-        transactionId = "-";
-    } else {
-        transactionId = invoice.transactionId;
+function invoiceAsTableRow(contract) {
+    return function (invoice) {
+        var transactionId;
+        if (invoice.transactionId == "null") {
+            transactionId = "-";
+        } else {
+            transactionId = invoice.transactionId;
+        }
+        var status;
+        if (transactionId == "-") {
+            status = "Active";
+        } else {
+            status = "Paid";
+        }
+        var pdfHref = "/api/contributor/contracts/"
+            + contract.id.repoFullName
+            + "/invoices/" + invoice.id
+            + "/pdf?role=" + contract.id.role;
+        return [
+            invoice.id,
+            invoice.createdAt.split('T')[0],
+            invoice.amount,
+            status,
+            "<a href='" + pdfHref + "' class='downloadInvoice'>" + "<i class='fa fa-file-pdf-o fa-lg'></i></a>"
+        ];
     }
-    var status;
-    if(transactionId == "-") {
-        status = "Active";
-    } else {
-        status = "Paid";
-    }
-    var pdfHref = "/api/contributor/contracts/"
-        + contract.id.repoFullName
-        + "/invoices/" + invoice.id
-        +"/pdf?role=" + contract.id.role;
-    return "<tr>" +
-        "<td>" + invoice.id + "</td>" +
-        "<td>" + invoice.createdAt.split('T')[0] + "</td>"  +
-        "<td>" + invoice.amount + "</td>" +
-        "<td>" + status + "</td>" +
-        "<td><a href='" +pdfHref+ "' class='downloadInvoice'>" + "<i class='fa fa-file-pdf-o fa-lg'></i>" + "</a></td>" +
-        "</tr>"
 }
 
 /**
@@ -270,21 +254,23 @@ function markContractForRemoval(contract, removeButton) {
         + "/mark?role=" + contract.id.role,
         {
             type: "DELETE",
-            success: function(marked) {
+            success: function (marked) {
                 var toolTipMessage = "This contract has been marked for removal on " + marked.markedForRemoval + ". "
                     + "No more tasks will be assigned to it and it will be removed after 30 days. "
                     + "Contact the PO if you want to have it reactivated."
                 removeButton.replaceWith("<i class='fa fa-exclamation-circle fa-lg' style='color:red;' aria-hidden='true' "
                     + "data-toggle='tooltip' data-placement='top' "
                     + "data-original-title='" + toolTipMessage + "'>"
-                    +"</i>");
-                $('[data-toggle="tooltip"]').tooltip();
+                    + "</i>");
+                $('[data-toggle="tooltip"]').tooltip({
+                    boundary: 'window'
+                });
             },
             error: function () {
                 alert("Something went wrong. Please refresh the page and try again.")
                 removeButton.on(
                     "click",
-                    function(event) {
+                    function (event) {
                         event.preventDefault();
                         confirmDialog
                             .create("Are you sure you want to remove this contract?")
@@ -308,13 +294,13 @@ function getPayoutMethods() {
             statusCode: {
                 200: function (payoutMethods) {
                     $("#loadingPayoutMethods").hide();
-                    if(payoutMethods.length == 0) {
+                    if (payoutMethods.length == 0) {
                         $(".no-payout-methods").show();
                     } else {
                         var method = payoutMethods[0];
                         $("#accountId").html(method.identifier);
                         var requirements = method.account.requirements.currently_due;
-                        if(method.account.details_submitted && requirements.length == 0) {
+                        if (method.account.details_submitted && requirements.length == 0) {
                             $("#stripeActive").show();
                             $("#stripeDashboardFormDiv").show();
                             $("#onboardingNeeded").hide();
@@ -324,7 +310,7 @@ function getPayoutMethods() {
                         } else {
                             $("#accountBadge").addClass("badge-danger")
                             $("#accountBadge").html("action required")
-                            if(method.account.details_submitted == false) {
+                            if (method.account.details_submitted == false) {
                                 $("#onboardingNeeded").show();
                                 $("#stripeRequirements").hide();
                                 $("#stripeActive").hide();
