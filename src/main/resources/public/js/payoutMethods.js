dbfe8df33ab3d679c76ee5018ffa38fe7814cd26
Lumi3011 <lumi.3011@yahoo.ro>
2021-03-15 19:44:38 +0200

    started the delete of payout methods

--- src/main/resources/public/js/payoutMethods.js
+++ src/main/resources/public/js/payoutMethods.js
@@ -16,7 +16,7 @@ $(document).ready(
                 }
             }
         );
-        $("#createStripeConnectAccountForm").submit(
+        $("#create ConnectAccountForm").submit(
             function(e) {
                 e.preventDefault();
                 var valid = true;
@@ -129,6 +129,34 @@ $(document).ready(
                     }
                 );
             }
+        );
+        $('#stripeDeleteButton').submit(
+                function(e) {
+                    e.preventDefault();
+                    $.ajax({
+                        type: "DELETE",
+                        url: "/api/contributor/payoutmethods/stripe",
+                        data: form.serealize(),
+                        success: function(response){
+                            $(".payout-methods").hide();
+                            $(".no-payout-methods").show();
+                        },
+                        error: function(jqXHR, error, errorThrown){
+                            if(jqXHR.status && jqXHR.status == 400){
+                                console.error("Bad Request: " + jqXHR.responseText);
+                                $(".browser-error").show();
+                            } else {
+                                console.log("Server error status: " + jqXHR.status);
+                                console.log("Server error: " + jqXHR.responseText);
+                                alert(
+                                    "Something went wrong (" + jqXHR.status + ")." +
+                                    "Please, try again later."
+                                );
+                            }
+                            
+                        }
+                    });
+                }
         );
         $("#stripeDashboardForm").submit(
             function(e) {
