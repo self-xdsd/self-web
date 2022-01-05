fbe12b5293632d9ef97b1016831c872fb861bbdd
amihaiemil <amihaiemil@gmail.com>
2022-01-05 18:09:56 +0200

    gross/net/totalVat numbers

--- src/app/platform-invoices-page/platform-invoices-page.component.ts
+++ src/app/platform-invoices-page/platform-invoices-page.component.ts
@@ -38,4 +38,26 @@ export class PlatformInvoicesPageComponent implements OnInit {
       platformInvoices => this.platformInvoices = platformInvoices
     );
   }
+
+  getTotal() {
+    let grossRevenue = 0;
+    let netRevenue = 0;
+    let totalVat = 0;
+    if(this.platformInvoices) {
+      this.platformInvoices.forEach(
+        platformInvoice => {
+          grossRevenue += platformInvoice.total;
+          netRevenue += platformInvoice.commission;
+          if(platformInvoice.vat >= 0) {
+            totalVat += platformInvoice.vat;
+          }
+        }
+      )
+    }
+    return {
+      grossRevenue: grossRevenue,
+      netRevenue: netRevenue,
+      totalVat: totalVat
+    }
+  }
 }
