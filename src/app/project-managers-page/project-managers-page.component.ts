6d829320986983ec85ddbe5a9ffb177cbcc0b450
amihaiemil <amihaiemil@gmail.com>
2022-01-05 19:45:35 +0200

    project managers page scaffolding

--- src/app/project-managers-page/project-managers-page.component.ts
+++ src/app/project-managers-page/project-managers-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ProjectManager} from "../projectManager";
+import {Router} from "@angular/router";
+import {ProjectManagersService} from "../project-managers.service";
+import {UserService} from "../user.service";
 
 @Component({
   selector: 'app-project-managers-page',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectManagersPageComponent implements OnInit {
 
-  constructor() { }
+  projectManagers?: ProjectManager[];
+
+  constructor(
+    private router: Router,
+    private projectManagersService: ProjectManagersService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
+    this.userService.getAuthenticatedUser().subscribe(
+      user => {
+        if(user && user.role === 'admin') {
+          this.loadProjectManagers();
+        } else {
+          this.router.navigateByUrl("/");
+        }
+      }
+    )
   }
 
+  loadProjectManagers(): void {
+    this.projectManagersService.getProjectManagers().subscribe(
+      projectManagers => this.projectManagers = projectManagers
+    );
+  }
 }
