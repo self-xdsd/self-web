16479c192edb98ef75fd891ff260835dc068f9a5
Valeriy Vyrva <valery1707@gmail.com>
2022-10-28 10:57:45 +0300

    Stripe countries: added Croatia

--- src/main/java/com/selfxdsd/selfweb/api/input/validators/ValidCountryValidator.java
+++ src/main/java/com/selfxdsd/selfweb/api/input/validators/ValidCountryValidator.java
@@ -43,7 +43,7 @@ public final class ValidCountryValidator
         "AU", "AT", "BE", "BG", "CA", "CY", "CZ", "DK", "EE", "FI", "FR",
         "DE", "GR", "HK", "HU", "IE", "IT", "JP", "LV", "LT", "LU", "MT",
         "NL", "NZ", "NO", "PL", "PT", "RO", "SG", "SK", "SI", "ES", "SE",
-        "CH", "GB", "US", "MY", "MX", "BR"
+        "CH", "GB", "US", "MY", "MX", "BR", "HR"
     );
 
     @Override
