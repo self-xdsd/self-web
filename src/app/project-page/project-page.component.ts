8dc4ca98e80d29244bf98d0410f01822f86b211c
Mihai Andronache <amihaiemil@Mihais-MacBook-Pro.local>
2025-01-05 15:18:25 +0200

    project page tabs

--- src/app/project-page/project-page.component.ts
+++ src/app/project-page/project-page.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {UserService} from "../user.service";
 
 @Component({
   selector: 'app-project-page',
@@ -6,7 +8,27 @@ import {Component, Input} from '@angular/core';
   styleUrl: './project-page.component.css',
   standalone: false
 })
-export class ProjectPageComponent {
+export class ProjectPageComponent implements OnInit {
   @Input() owner!: string;
   @Input() name!: string;
+  provider?: string;
+  activeTab?: string;
+
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.activeTab = 'overview';
+    this.userService.getAuthenticatedUser().subscribe(
+      user => {
+        if(user) {
+          this.provider = user.provider;
+        } else {
+          this.router.navigateByUrl("/");
+        }
+      }
+    )
+  }
 }
